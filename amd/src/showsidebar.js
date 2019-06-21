44cdbd567437513e9d9913f4ad68955152e4c1fb
Gareth Barnard <gjbarnard@gmail.com>
2019-06-21 11:42:37 +0100

    #149 wip.

--- amd/src/showsidebar.js
+++ amd/src/showsidebar.js
@@ -11,19 +11,16 @@ define(['jquery', 'core/log'], function($, log) {
 
                 var sidePostClosed = true;
                 var sidePost = $('#block-region-side-post');
-                var showSideBar = $('#showsidebaricon');
                 var showSideBarIcon = $('#showsidebaricon i.fa');
                 if (typeof sidePost != 'undefined') {
-                    showSideBar.click(function() {
+                    $('#showsidebaricon').click(function() {
                         if (sidePostClosed === true) {
                             sidePost.addClass('sidebarshown');
-                            showSideBar.addClass('sidebarshown');
                             showSideBarIcon.removeClass('fa-chevron-left');
                             showSideBarIcon.addClass('fa-chevron-right');
                             sidePostClosed = false;
                         } else {
                             sidePost.removeClass('sidebarshown');
-                            showSideBar.removeClass('sidebarshown');
                             showSideBarIcon.removeClass('fa-chevron-right');
                             showSideBarIcon.addClass('fa-chevron-left');
                             sidePostClosed = true;
