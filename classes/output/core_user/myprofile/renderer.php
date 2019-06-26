c3814dd4d1863763a3699c2845bde9f3a49d955f
Gareth Barnard <gjbarnard@gmail.com>
2019-06-26 18:08:36 +0100

    #143 wip.

--- classes/output/core_user/myprofile/renderer.php
+++ classes/output/core_user/myprofile/renderer.php
@@ -29,6 +29,7 @@ namespace theme_adaptable\output\core_user\myprofile;
 defined('MOODLE_INTERNAL') || die;
 
 use core_user\output\myprofile\tree;
+use html_writer;
 
 /**
  * myprofile renderer.
@@ -43,8 +44,44 @@ class renderer extends \core_user\output\myprofile\renderer {
      * @return string
      */
     public function render_tree(tree $tree) {
-        error_log('autoloaded!');
-        return parent::render_tree($tree);
+        static $categorycolone = array('contact');
+        $categories = array();
+        foreach ($tree->categories as $category) {
+            $markup = $this->render($category);
+            if (!empty($markup)) {
+                $categories[$category->name] = $markup;
+            }
+        }
+
+        $output = html_writer::start_tag('div', array('class' => 'profile_tree row'));
+
+        $output .= html_writer::start_tag('div', array('class' => 'col-md-4')); // Col one.
+
+        $output .= html_writer::start_tag('div', array('class' => 'row'));
+        foreach ($categorycolone as $categoryname) {
+            if (!empty($categories[$categoryname])) {
+                $output .= html_writer::start_tag('div', array('class' => 'col-12 '.$categoryname));
+                $output .= $categories[$categoryname];
+                unset($categories[$categoryname]);
+                $output .= html_writer::end_tag('div');
+            }
+        }
+        $output .= html_writer::end_tag('div');
+        $output .= html_writer::end_tag('div');
+
+        $output .= html_writer::start_tag('div', array('class' => 'col-md-8')); // Col two.
+        $output .= html_writer::start_tag('div', array('class' => 'row'));
+        foreach ($categories as $categoryname => $category) {
+            $output .= html_writer::start_tag('div', array('class' => 'col-12 '.$categoryname));
+            $output .= $category;
+            $output .= html_writer::end_tag('div');
+        }
+        $output .= html_writer::end_tag('div');
+        $output .= html_writer::end_tag('div');
+
+        $output .= html_writer::end_tag('div');
+
+        return $output;
     }
 
 }
